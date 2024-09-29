2156da47d27cd652ecbbbd36b2cb7b9047eb9eab
William Justice Davis <hvusvztc770@gmail.com>
2024-09-29 08:40:52 +0300

    react skeleton - first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>place holder for / page</h1>}/>
+        <Route path="/home" element={<h1>place holder for home page</h1>}/>
+        <Route path="/login" element={<h1>place holder for login page</h1>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
