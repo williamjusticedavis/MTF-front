4b261d33a9c5f87203cf2d3b7f87f97b039bdf2e
William Justice Davis <hvusvztc770@gmail.com>
2024-09-29 09:09:53 +0300

    added testing url + folder name changed

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <Route path="/" element={<h1>place holder for / page</h1>}/>
         <Route path="/home" element={<h1>place holder for home page</h1>}/>
         <Route path="/login" element={<h1>place holder for login page</h1>}/>
+        <Route path="/test" element={<h1>place holder for testing page</h1>}/>
       </Routes>
       </BrowserRouter>
     </>
