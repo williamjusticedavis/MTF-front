0e85d294fca9239392ae5109d74107ca05d039e6
israel <hartl123452019@gmail.com>
2024-10-02 18:06:05 +0300

    added functionaleti to Downloduserlist

--- src/components/DownloadUserList.tsx
+++ src/components/DownloadUserList.tsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import axios from 'axios'
 
 const DownloadUserList: React.FC = () => {
+  const downloadUsers: () => Promise<void> = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/users/export-users', {
+        responseType: 'blob',
+      });
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'users.xlsx'); // שם הקובץ
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.error('Error downloading the file', error);
+    }
+  };
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full space-y-4">
       <p className="text-gray-700 text-lg font-medium">Are you sure you want to download the user list?</p> 
@@ -8,7 +25,7 @@ const DownloadUserList: React.FC = () => {
         <button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors">
           Back
         </button>
-        <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors">
+        <button onClick={downloadUsers} className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors">
           Download
         </button>
       </div>
