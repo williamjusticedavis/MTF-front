dfdacb420e083dc93b5b0d947c3429430552d1b5
israel <israeledrey770@gmail.com>
2024-10-07 10:46:13 +0300

    lejdo

--- src/components/DeleteUser.tsx
+++ src/components/DeleteUser.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { MdDelete } from "react-icons/md";
 
 const DeleteUser: React.FC = () => {
+  
+  
+
   return (
     <div>
      <button className='text-gray-500 transition-transform duration-200 transform hover:text-gray-800  hover:scale-150 focus:scale-150 focus:outline-none'><MdDelete /></button>
