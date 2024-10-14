da35098de71a1af84530635fc4accae202057703
William Justice Davis <hvusvztc770@gmail.com>
2024-10-14 10:26:39 +0300

    added map

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Otp from './pages/Otp';
 import NotFound from './components/NotFound';
 import { Provider } from "react-redux";
 import store from './redux/store';
+import Map from './pages/Map';
 
 const App: React.FC = () => {
   return (
@@ -17,7 +18,8 @@ const App: React.FC = () => {
           <Route path="/About" element={<h1>this is About page</h1>} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<h1>place holder for testing page</h1>} />
-          
+          <Route path="/map" element={<Map/>} />
+          // wh
           {/* Unprotected /login/otp route */}
           <Route path="/login/otp" element={<Otp />} />
 
